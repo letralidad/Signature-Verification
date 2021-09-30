3ee1defa73c9dca9ee57a2e9a64212434398f00f
Keano Kim <75988307+mrslayer08@users.noreply.github.com>
2021-09-30 16:25:21 +0800

    Putting a canvas for signature
    Putting a canvas for signature

--- SignatureVerification/app/src/main/java/com/tnightmares/signatureverification/EnterSignature.java
+++ SignatureVerification/app/src/main/java/com/tnightmares/signatureverification/EnterSignature.java
@@ -1,14 +1,63 @@
 package com.tnightmares.signatureverification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class EnterSignature extends AppCompatActivity {
+import com.kyanogen.signatureview.SignatureView;
+
+public class EnterSignature extends AppCompatActivity implements View.OnClickListener {
+
+    CardView next, save, clear;
+    LinearLayout title;
+    ConstraintLayout forSignature;
+    SignatureView signatureCanvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_signature);
+
+        next = (CardView) findViewById(R.id.next);
+        save = (CardView) findViewById(R.id.save);
+        clear = (CardView) findViewById(R.id.clear);
+
+        title = findViewById(R.id.title);
+
+        forSignature = findViewById(R.id.forSignature);
+
+        signatureCanvas = findViewById(R.id.signatureCanvas);
+
+
+        next.setOnClickListener(this);
+        save.setOnClickListener(this);
+        clear.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()){
+            case R.id.next:
+                title.setVisibility(View.GONE);
+                forSignature.setVisibility(View.VISIBLE);
+                break;
+            case R.id.save:
+                Log.d("TAG", "onClick: save");
+                break;
+            case R.id.clear:
+                signatureCanvas.clearCanvas();
+                break;
+            default:
+                Log.d("TAG", "onClick: this is default");
+                break;
+        }
     }
 }
